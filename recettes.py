6a6c1932acf8869669478ae36e648d5b750c2ce8
Laurine <laurine.kmn@gmail.com>
2022-11-07 22:57:44 +0100

    Fonction without_one ok, fonction create_graph, fonction solve_my_query (pas sûre de garder)

--- recettes.py
+++ recettes.py
@@ -7,7 +7,6 @@ Laurine Komendanczyk
 
 from py2neo import Graph, Node, Relationship
 import pandas as pd
-# from pandas import DataFrame
 import os
 import numpy as np
 
@@ -20,23 +19,24 @@ os.chdir("D:/Documents/3A/bigdata/Recipes/recipes_neo4j/")
 
 # Import dataset
 df = pd.read_csv("./Data/recipes_data.csv", delimiter=",")
-# df = np.array(df)
 
 #%% --------------- SUBSET FOR TESTS -----------------
 
-df2 = df.copy()[1:200]
+SUBSET_SIZE = 200
+
+df2 = df.copy()[0:SUBSET_SIZE]
 
 df2['label'] = df2['label'].str.replace("'", "")
-colnames = list(df2.columns)
-colnames = colnames[1: ]
-df2.dtypes
+# colnames = list(df2.columns)
+# colnames = colnames[1: ]
+# df2.dtypes
 
-for col in colnames : 
-    # print(type(col))
-    df2.iloc[: ,1: ] = df2.iloc[: ,1: ].astype('int32')
-    # df2.iloc[:,1:].astype('int32', copy = False)
+# for col in colnames : 
+#     # print(type(col))
+#     df2.iloc[: ,1: ] = df2.iloc[: ,1: ].astype('int32')
+#     # df2.iloc[:,1:].astype('int32', copy = False)
 
-df2.dtypes
+# df2.dtypes
 
 #%% --------------- ARE THERE MISSING VALUES -----------------
 
@@ -46,84 +46,29 @@ df.isna().sum().sum()
 
 #%% ---------------NODES AND RELATIONSHIPS-----------------
 
-# Graph Cleaning before starting 
-graph.run("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r")
-
-
-# =================================
-# Creation of nodes for each recipe
-# =================================
-
-recipe = {}
-
-for index, row in df2.iterrows(): #index est un entier, rows est pd.Series
-    # print(index, type(index))
-    # print(row, type(row))
-    recipe[row['label']] = Node("Recipe",
-                                name = str(row['label'])
-                                )
-
-# =====================================
-# Creation of nodes for each ingredient 
-# =====================================
-
-ingredients = {}
-
-for name, col in df2.iteritems():
-    ingredients[name] = Node("Ingredient", 
-                             name = str(name))
-
-# ============
-# Relationship 
-# ============
- 
-Contain = Relationship.type("CONTAINS")
-
-for index, row in df2.iterrows():
-    r = recipe[row['label']]
+def create_graph():
+    # Graph Cleaning before start
+    graph.run("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r")
     
-    for ingredient in list(row.index):
-        i = ingredients[ingredient]
-        if row[ingredient] == 1: 
-            graph.create(Contain(r, i))
-
-
-
-#%% ---------------- Optimisation ---------------
-
-# Graph Cleaning before start
-graph.run("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r")
-
-
-# =========
-# Creation of nodes for each ingredient 
-# =========
-
-ingredients = {}
-
-for name, col in df.iteritems():
-    ingredients[name] = Node("Ingredient", 
-                             name = str(name))
-
-# =========================================
-# Creation of relationship with each recipe
-# =========================================
-
-Contain = Relationship.type("CONTAINS")
-
-recipe = {}
-
-for index, row in df.iterrows():
-    recipe[row['label']] = Node("Recipe",
-                                name=str(row['label']))
-    r = recipe[row['label']]
-    L = [i for i in list(row.index) if row[i] == 1]
+    ingredients = {}
+    recipe = {}
+    Contain = Relationship.type("CONTAINS")
     
-    for ingredient in L:
-        i = ingredients[ingredient]
-        if row[ingredient]==1: 
-            graph.create(Contain(r, i))
-
+    # =========================================
+    # Creation of the nodes and relatioships
+    # =========================================
+    
+    for index, row in df2.iterrows():
+        recipe[row['label']] = Node("Recipe", name=str(row['label']))
+        r = recipe[row['label']]
+        L = [i for i in list(row.index) if row[i] == 1]
+        for ingredient in L:
+            if ingredient not in ingredients :
+                ingredients[ingredient]= Node("Ingredient", name = str(ingredient))
+            i = ingredients[ingredient]
+            if row[ingredient]==1: 
+                graph.create(Contain(r, i))
+    
 
 #%% ---------------- Requêtes -------------------
 
@@ -167,7 +112,7 @@ def list_ingredients ():
     return (L)
 
 # =============
-# Visualisation
+# Interface
 # =============
 
 rq = "MATCH (r:Recipe)-[:CONTAINS]->(i:Ingredient) RETURN list(r, count(DISTINCT i)"
@@ -229,11 +174,10 @@ def get_recipes_without_one(my_ing):
 
     """
     L = str()
-    
-    rq = f"MATCH (r:Recipe)-[:CONTAINS]->(i:Ingredient) WHERE i.name NOT IN [\'{my_ing}\'] RETURN DISTINCT r"
+    rq = "MATCH (r:Recipe) WHERE NOT (r)-[:CONTAINS]->(:Ingredient {name:"+f"\'{my_ing}\'" + "}) RETURN DISTINCT r.name"
     data = graph.run(rq).data()
     for elem in data:
-        L = L + "\n" + str(elem['r']['name'])
+        L = L + "\n" + str(elem['r.name'])
     return L
 
 # Récupérer liste des recettes contenant un ingrédient donné
@@ -256,5 +200,24 @@ def get_recipes():
     for elem in data:
         L.append(elem['r']['name'])
     return L
+
+
+# Fonction renvoyant le résultat d'une requête Cypher entrée par l'utilisateur
+def solve_my_query(rq):
+    """
+    Renvoie le résultat d'une requête Cypher entrée par l'utilisateur
+    
+    Input : 
+        * rq : chaîne de caractère, requête Cypher portant sur le jeu de données 
+    
+    Output : 
+        * liste des résultats de la requête rq
+    """
+    data = graph.run(rq).data()
+    L = str()
+    for elem in data:
+        L = L + "\n" + str(elem.values())
+    return L    
+    
 
 
