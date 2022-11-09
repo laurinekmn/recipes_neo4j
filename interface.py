fee7c3f5a2e9c5f37d1dd3d7732184145a3bed76
Laurine <laurine.kmn@gmail.com>
2022-11-09 17:42:58 +0100

    Scrollbar pour les résultats de chaque requête fonctionnent!

--- interface.py
+++ interface.py
@@ -7,7 +7,7 @@ Created on Fri Nov  4 19:04:15 2022
 
 from tkinter import *
 # from onglets import create_VIZ, create_ING, create_REC
-from recettes import *
+# from recettes import *
 
 #%% --------------- COULEURS DE L'INTERFACE ------------------
 
@@ -16,11 +16,7 @@ COLOR_ACCENT1 = "#3d915f"
 COLOR_ACCENT2 = "#855f53"
 COLOR_ACCENT3 = "#608e85"
 
-#%%
-
-
 #%% ------------ SOUS-FENÊTRES DE L'INTERFACE-----------------
-
 
 def create_LIST():
     win = Toplevel(window)
@@ -30,9 +26,9 @@ def create_LIST():
     # Espaces entre les différents éléments 
     Spacetop = Label(win, text = "", height = 3, bg = COLOR_BG)
     Space0 = Label(win, text = "", height = 1, bg = COLOR_BG)
-    Space1 = Label(win, text = "", height = 3, bg = COLOR_BG)
-    Space2 = Label(win, text = "", height = 3, bg = COLOR_BG)
-    Space3 = Label(win, text = "", height = 3, bg = COLOR_BG)
+    Space1 = Label(win, text = "", height = 2, bg = COLOR_BG)
+    Space2 = Label(win, text = "", height = 1, bg = COLOR_BG)
+    Space3 = Label(win, text = "", height = 2, bg = COLOR_BG)
 
     # Titre de la fenêtre
     Main_title = Label(win, text = "List of ingredients", 
@@ -65,6 +61,8 @@ def create_LIST():
     def update_L_ing ():
         v = var.get()
         L_ing.set(get_ingredients(v))
+        canevas0.itemconfigure(results, text = L_ing.get()) #modif résultats
+
     
     update = Button(win, 
                     text = "Update the list", 
@@ -72,13 +70,21 @@ def create_LIST():
                     bg = COLOR_ACCENT3, 
                     fg = "white",
                     command = update_L_ing)
+
+   # Création d'un canva pour afficher le résultat de la requête
+    cadre0 = Frame(win, width = 500, height = 400)
     
-    # Liste des ingrédients de la recette sélectionnée
-    ListIng = Label(win, 
-                    font = ("Arial", 13), 
-                    bg = COLOR_BG, 
-                    fg = "black",
-                    textvariable = L_ing)
+    canevas0 = Canvas(cadre0, bg = COLOR_BG, 
+                      width = 500, height = 200)
+    
+    ascenseur0 = Scrollbar(cadre0, 
+                           orient = 'vertical', 
+                           command = canevas0.yview)
+    
+    canevas0.config(scrollregion = (0,0,500,500),
+                    yscrollcommand = ascenseur0.set)
+
+
     
     # "Construction" de la fenêtre avec pack 
     Spacetop.pack()
@@ -90,8 +96,17 @@ def create_LIST():
     Space2.pack()
     update.pack()
     Space3.pack()
-    ListIng.pack()
+    cadre0.pack()
+    ascenseur0.pack(side = RIGHT, fill = "y")
+    canevas0.pack(side = LEFT, expand = True)
+    
+    # Résultats 
+    results = canevas0.create_text(12, 12, 
+                                 font = ("Arial", 12) ,
+                                 text = L_ing.get(), 
+                                 anchor = NW)
 
+#%% 
 
 def create_ONE():
     
@@ -101,9 +116,9 @@ def create_ONE():
     
     Spacetop = Label(win, text = "", height = 3, bg = COLOR_BG)
     Space0 = Label(win, text = "", height = 1, bg = COLOR_BG)
-    Space1 = Label(win, text = "", height = 3, bg = COLOR_BG)
-    Space2 = Label(win, text = "", height = 3, bg = COLOR_BG)
-    Space3 = Label(win, text = "", height = 3, bg = COLOR_BG)
+    Space1 = Label(win, text = "", height = 2, bg = COLOR_BG)
+    Space2 = Label(win, text = "", height = 1, bg = COLOR_BG)
+    Space3 = Label(win, text = "", height = 2, bg = COLOR_BG)
 
     Main_title = Label(win, text = "Specific ingredient in a recipe", 
                        font = ("Arial", 28, "bold"), 
@@ -135,6 +150,8 @@ def create_ONE():
     def update_L_rec ():
         v = var.get()
         L_rec.set(get_recipes_one(v))
+        canevas0.itemconfigure(results, text = L_rec.get()) #modif résultats
+
     
     update = Button(win, 
                     text = "Update the list", 
@@ -143,14 +160,18 @@ def create_ONE():
                     fg = "white",
                     command = update_L_rec)
     
+    # Création d'un canva pour afficher le résultat de la requête
+    cadre0 = Frame(win, width = 500, height = 400)
     
-    # Liste des ingrédients de la recette sélectionnée
-    ListRec = Label(win, 
-                    font = ("Arial", 13), 
-                    bg = COLOR_BG, 
-                    fg = "black",
-                    textvariable = L_rec)
+    canevas0 = Canvas(cadre0, bg = COLOR_BG, 
+                      width = 500, height = 200)
     
+    ascenseur0 = Scrollbar(cadre0, 
+                           orient = 'vertical', 
+                           command = canevas0.yview)
+    
+    canevas0.config(scrollregion = (0,0,750, 750),
+                    yscrollcommand = ascenseur0.set)
     
     Spacetop.pack()
     Main_title.pack() 
@@ -161,9 +182,17 @@ def create_ONE():
     Space2.pack()
     update.pack()
     Space3.pack()
-    ListRec.pack()
-
+    cadre0.pack()
+    ascenseur0.pack(side = RIGHT, fill = "y")
+    canevas0.pack(side = LEFT, expand = True)
     
+    # Résultats 
+    results = canevas0.create_text(12, 12, 
+                                 font = ("Arial", 12) ,
+                                 text = L_rec.get(), 
+                                 anchor = NW)
+    
+#%%
 
 def create_WITHOUT_ONE():
     
@@ -173,9 +202,9 @@ def create_WITHOUT_ONE():
     
     Spacetop = Label(win, text = "", height = 3, bg = COLOR_BG)
     Space0 = Label(win, text = "", height = 1, bg = COLOR_BG)
-    Space1 = Label(win, text = "", height = 3, bg = COLOR_BG)
-    Space2 = Label(win, text = "", height = 3, bg = COLOR_BG)
-    Space3 = Label(win, text = "", height = 3, bg = COLOR_BG)
+    Space1 = Label(win, text = "", height = 2, bg = COLOR_BG)
+    Space2 = Label(win, text = "", height = 1, bg = COLOR_BG)
+    Space3 = Label(win, text = "", height = 2, bg = COLOR_BG)
 
     Main_title = Label(win, text = "Recipes without one ingredient", 
                        font = ("Arial", 28, "bold"), 
@@ -207,28 +236,27 @@ def create_WITHOUT_ONE():
     def update_L_rec ():
         v = var.get()
         L_rec.set(get_recipes_without_one(v))
-    
+        canevas0.itemconfigure(results, text = L_rec.get()) #modif résultats
+
     update = Button(win, 
                     text = "Update the list", 
                     font = ("Arial", 16, "bold"), 
                     bg = COLOR_ACCENT1, 
                     fg = "white",
-                    command = update_L_rec)
+                    command = update_L_rec)   
     
+    # Création d'un canva pour afficher le résultat de la requête
+    cadre0 = Frame(win, width = 500, height = 400)
     
-    # Liste des ingrédients de la recette sélectionnée
-    ListRec = Label(win, 
-                    font = ("Arial", 13), 
-                    bg = COLOR_BG, 
-                    fg = "black",
-                    textvariable = L_rec)
+    canevas0 = Canvas(cadre0, bg = COLOR_BG, 
+                      width = 500, height = 200)
     
-    cadre0 = Frame(win, width =400, height =400)
-    canevas0 = Canvas(cadre0, bg =COLOR_BG, width =400, height =200, scrollregion =(0, 0, 250, 250))
-    ascenseur0 = Scrollbar(cadre0)
-    ascenseur0.config(command = canevas0.yview)
-    canevas0.config(width =250, height = 200)
-    canevas0.config(yscrollcommand = ascenseur0.set)
+    ascenseur0 = Scrollbar(cadre0, 
+                           orient = 'vertical', 
+                           command = canevas0.yview)
+    
+    canevas0.config(scrollregion = (0,0,4000, 4000),
+                    yscrollcommand = ascenseur0.set)
     
     
     Spacetop.pack()
@@ -240,15 +268,15 @@ def create_WITHOUT_ONE():
     Space2.pack()
     update.pack()
     Space3.pack()
-    
     cadre0.pack()
-    ascenseur0.pack(side = RIGHT, fill = Y)
-    canevas0.pack(side = LEFT, expand =True)
-    texte1 =canevas0.create_text(12, 12, textvariable = L_rec, anchor = NW)
-
-
-
-    ListRec.pack()
+    ascenseur0.pack(side = RIGHT, fill = "y")
+    canevas0.pack(side = LEFT, expand = True)
+    
+    # Résultats 
+    results = canevas0.create_text(12, 12, 
+                                 font = ("Arial", 12) ,
+                                 text = L_rec.get(), 
+                                 anchor = NW)
 
 
 #%% ------------------ FENETRE D'ACCUEIL ---------------------
