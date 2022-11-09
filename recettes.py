36f46fe5d18e481459e16839d9fa30a88ce23f57
Laurine <laurine.kmn@gmail.com>
2022-11-09 22:25:45 +0100

    Fonctions commentées + exemples d'application + code clean

--- recettes.py
+++ recettes.py
@@ -2,6 +2,11 @@
 PROJET NEO4J - RECETTES DE CUISINE
 Pour le 10 novembre 2022
 
+    * Connexion à Neo4j 
+    * Création du graphe 
+    * Création d'un subset
+    * Requêtes dans la base de données en graphe
+
 Laurine Komendanczyk
 """
 
@@ -29,36 +34,35 @@ def create_subset(SUBSET_SIZE):
 
 create_subset(SUBSET_SIZE)
 
-#%% --------------- ARE THERE MISSING VALUES -----------------
+#%% --------------- VALEURS MANQUANTES ? -----------------
 
-df.isnull().sum().sum()
-df.isna().sum().sum()
-
+# df.isnull().sum().sum()
+# df.isna().sum().sum()
 
 #%% ---------------NODES AND RELATIONSHIPS-----------------
 
 def create_graph(df):
-    # Graph Cleaning before start
+    # Nettoyage du graphe
     graph.run("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r")
     
     ingredients = {}
     recipe = {}
     Contain = Relationship.type("CONTAINS")
     
-    # =========================================
-    # Creation of the nodes and relatioships
-    # =========================================
+    # ====================================
+    # Création des noeuds et des relations 
+    # ====================================
     
     for index, row in df.iterrows():
-        recipe[row['label']] = Node("Recipe", name=str(row['label']))
+        recipe[row['label']] = Node("Recipe", name=str(row['label'])) # création noeud
         r = recipe[row['label']]
         L = [i for i in list(row.index) if row[i] == 1]
         for ingredient in L:
             if ingredient not in ingredients :
-                ingredients[ingredient]= Node("Ingredient", name = str(ingredient))
+                ingredients[ingredient]= Node("Ingredient", name = str(ingredient)) # création noeud
             i = ingredients[ingredient]
             if row[ingredient]==1: 
-                graph.create(Contain(r, i))
+                graph.create(Contain(r, i)) # création relation
     
 
 #%% ---------------- Requêtes -------------------
@@ -67,6 +71,8 @@ def create_graph(df):
 # Listes de base
 # ==============
  
+# >>>> Récupérer la liste des recettes de la base de données
+
 def list_recipes ():
     """
     Renvoie la liste de tous les noms de recettes de la base de données
@@ -84,7 +90,13 @@ def list_recipes ():
         L.append(elem['r']['name'])
     return (L)
 
+# Exemple
+print("===============================================")
+print ("Liste des recettes présentes dans le graphe (5 premiers éléments affichés)")
+print(list_recipes()[0:5])
 
+
+# >>>> Récupérer la liste des ingrédients de la base de données
 def list_ingredients ():
     """
     Renvoie la liste de tous les noms d'ingrédients de la base de données
@@ -102,18 +114,17 @@ def list_ingredients ():
         L.append(elem['i']['name'])
     return (L)
 
+# Exemple
+print("===============================================")
+print ("Liste des ingrédients présents dans le graphe (5 premiers éléments affichés)")
+print(list_ingredients()[0:5])
+
+
 # ===================================
 # Requêtes utilisées dans l'interface
 # ===================================
 
-rq = "MATCH (r:Recipe)-[:CONTAINS]->(i:Ingredient) RETURN list(r, count(DISTINCT i)"
-
-
-
-# Récupérer liste des ingrédients d'une recette choisie 
-
-my_r = "Hazelnut Chicken"
-
+# >>>> Récupérer la liste des ingrédients d'une recette choisie 
 
 def get_ingredients (my_r):
     """
@@ -133,6 +144,13 @@ def get_ingredients (my_r):
         L = L + "\n" + str(elem['i']['name'])
     return L[1:]
 
+# Exemple
+print("===============================================")
+print ("Liste des ingrédients de la recette 'Ranch Dipper'")
+print(get_ingredients("Ranch Dipper"))
+
+
+# >>>> Récupérer la liste des recettes contenant un ingrédient choisi
 
 def get_recipes_one(my_ing):
     """
@@ -153,6 +171,14 @@ def get_recipes_one(my_ing):
         L = L + "\n" + str(elem['r']['name'])
     return L
 
+# Exemple
+print("===============================================")
+print ("Liste des recettes contenant l'ingrédient 'sour cream'")
+print(get_recipes_one("sour cream"))
+
+
+# >>>> Récupérer la liste des recettes ne contenant pas un ingrédient choisi
+
 def get_recipes_without_one(my_ing):
     """
     Renvoie la liste de toutes les recettes ne contenant pas l'ingrédient my_ing donné en entrée 
@@ -171,11 +197,16 @@ def get_recipes_without_one(my_ing):
         L = L + "\n" + str(elem['r.name'])
     return L
 
+# Exemple
+print("===============================================")
+print ("Liste des recettes contenant ne contenant pas l'ingrédient 'sour cream'")
+print(get_recipes_without_one("sour cream"))
+
 # ================
 # Requêtes en plus
 # ================
 
-# >>>> Récupérer liste des recettes contenant un ingrédient donné
+# >>>> Récupérer liste des recettes contenant une liste d'ingrédients donnés
 def get_recipes(L_ing):
     """
     Renvoie la liste de toutes les recettes contenant les ingrédients de L_ing donnés en entrée 
@@ -205,10 +236,44 @@ def get_recipes(L_ing):
 # Exemple
 print("===============================================")
 print ("Recettes contenant les ingrédients 'olive oil' et 'red wine vinegar'")
-get_recipes(["olive oil", "red wine vinegar"]) # Beet Salad
+print(get_recipes(["olive oil", "red wine vinegar"])) 
+
+
+# >>>> Récupérer liste des recettes ne contenant pas une liste d'ingrédients donnés
+def get_recipes_without_many(L_ing):
+    """
+    Renvoie la liste de toutes les recettes ne contenant pas les ingrédients de L_ing donnés en entrée 
+    
+    Input : 
+        * L_ing : liste (ingrédients)
+    
+    Output : 
+        * L : liste (noms de recettes)
+    """
+    L = list()
+    if len(L_ing) == 0 : 
+        return(print("La liste doit contenir au moins un ingrédient"))
+    if len(L_ing)==1 : 
+        rq = "MATCH (r:Recipe)-[:CONTAINS]->(i:Ingredient) WHERE NOT (r)-[:CONTAINS]-> (:Ingredient {name:" + f"\'{L_ing[0]}\'" + "}) RETURN DISTINCT r.name"
+    else : 
+        rq = "MATCH (r:Recipe)-[:CONTAINS]->(i:Ingredient) WHERE NOT (r)-[:CONTAINS]-> (:Ingredient {name:" + f"\'{L_ing[0]}\'" + "})"
+        for k in range(1,len(L_ing)):
+            rq = rq + "AND NOT (r)-[:CONTAINS]-> (:Ingredient {name:" + f"\'{L_ing[k]}\'" + "})"
+        rq = rq + "RETURN DISTINCT r.name"
+
+    data = graph.run(rq).data()
+    for elem in data:
+        L.append(elem['r.name'])
+    return (L)
+
+# Exemple
+print("===============================================")
+print ("Recettes contenant ne contenant pas les ingrédients 'sour cream' et 'soy sauce' (Affichage des 5 premières recettes)")
+print(get_recipes_without_many(["sour cream", "soy sauce"])[0:5]) 
 
 # >>>> Fonction renvoyant le résultat d'une requête Cypher entrée par l'utilisateur
-def solve_my_query(rq):
+
+def print_my_query(rq):
     """
     Renvoie le résultat d'une requête Cypher entrée par l'utilisateur
     
@@ -216,13 +281,13 @@ def solve_my_query(rq):
         * rq : chaîne de caractère, requête Cypher portant sur le jeu de données 
     
     Output : 
-        * liste des résultats de la requête rq
+        * /
+        print des résultats 
     """
     data = graph.run(rq).data()
-    L = str()
     for elem in data:
-        L = L + "\n" + str(elem.values())
-    return L    
+        print(elem.values())
     
-
-
+print("===============================================")
+print ("Exemple d'utilisation de la fonction print_my_query")
+print_my_query("match (r:Recipe) where (r)-[:CONTAINS]->(:Ingredient {name:'sour cream'}) return distinct r.name ")
